5a3638cf5516545db7717f0fdea2a4adf8ab1d71
Ket0022 <ket0022@mix.wvu.edu>
2023-01-13 10:45:25 -0500

    localhost being ridiculous

---
+++ Task_01/task01.r
@@ -0,0 +1,32 @@
+library(swirl)
+swirl()
+Kylie
+1
+1
+1
+1
+<- 5+7
+x
+y <- x - 3
+y
+c(1.1, 9, 3.14)
+z
+c(z, 555, z)
+z*2 + 100
+sqrt (z-1)
+z <- sqrt(z-1)
+my_sqrt <- sqrt (z-1)
+1
+2
+my_sqrt <- sqrt (z-1)
+1
+c(1 ,2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+z * 2 + 1000
+z * 2 + 1000
+my_div
+1
+Ket0022@mix.wvu.edu
+
+
+### TEST

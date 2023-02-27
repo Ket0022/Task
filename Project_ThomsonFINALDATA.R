f4f618dfe90bbdacf3d9d6ea52019df4ca5cd8cf
Ket0022 <122411699+Ket0022@users.noreply.github.com>
2023-02-26 19:55:36 -0500

    Add files via upload

---
+++ Project_ThomsonFINALDATA.R
@@ -0,0 +1,5 @@
+setwd("C://Users//kthom//Desktop//Evolution//Tasks//Project")
+Data<-read.csv("Project.csv")
+head(Data)
+
+# Brown colored coat alleles will increase in frequency in increased temperature simulation. A model for this hypothesis could be X is the frequency of each allele over Y time with temperature.

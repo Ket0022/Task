f74320417ea54b5221b8b2ce952ce5fe5875a7a4
Ket0022 <ket0022@mix.wvu.edu>
2023-04-20 23:37:44 -0400

    Task 09 upload

--- Task_08/Task_08.R
+++ Task_08/Task_08.R
@@ -10,58 +10,18 @@ treelist <-c()
 for (i in 1:100){
   births[i]<- runif(1)
   fractions[i]<- runif(1)
-  trees[[i]] <-pbtree(n=100, b=births[i], d=births[i]*fractions[i])
-  random[[i]] <- births[i]
-  random2[[i]] <- (fractions[i])
-  treelist[[i]] <- mean(trees[[i]]$edge.length)
+  trees[[i]]<- pbtree(n=100, b=births[i], d=births[i]*fractions[i])
+  random[[i]]<- births[i]
+  random2[[i]]<- (fractions[i])
+  treelist[[i]]<- mean(trees[[i]]$edge.length)
 }
-plot(Trees[[i]])
-sapply(trees, Ntip)
-tips <-log(sapply(trees,Ntip))
-random3<-unlist(random)
-head(tips)
-pdf("log tips & Diversification question 4. pdf")
-plot(tips, xlab="log of tips", ylab=" net diversification")
-dev.off()
-?cor
-cor(tips,random3)
-random4<-unlist(random2)
-treelist2<-unlist(treelist)
-plot(treelist2, random3, ylab="speciation rate", xlab="Average branch length", pch=4)
-pdf("speciation rate and average brnach length.pdf")
-plot(treelist2, random3, ylab="speciation rate", xlab="Average branch length", pch=1)
-cor(treelist2, random4)
-Tree<-trees[[which.max(tips)]]
-pdf("Tree.pdf")
-plot(Tree)
-rates<-vector()
-traits<-c()
-meantraits<-c()
-vartraits<-c()
-for( i in 1:100){
-  rates[i]<- runif(1)
-  traits[[i]]<- fastBM(tree=Tree, sig2=rates[i])
-  mean_traits <- sapply(traits, mean)
-  vartraits[[i]]<-VAR(TRAITS[[i]])
-  cor(mean_trait, rates)
-  vartraits<-unlist(vartraits)
-  pdf("rate of vartraits.pdf")
-  plot(vartraits,rates)
-  cor(vartraits,rates)
-  pdf("08_plot.pdf")
-  plot(traits[[i]], traits[[2]])
-  cor(traits[[1]], traits[[2]])
-  traitMat<-cbind(traits[[1]], traits[[4]])
-  #Extracredit:
-  pdf("08EC plot.pdf")
-  phylomorphospace(Tree, traitMat, xlab="Trait 1", ylab="Trait 2")
 #Question4
 # the net diversification increases as the log of the number of tips increases as an exponential curve.
 div_rate <- sapply(trees,function(x) length(x$tip.label))
 log_tips <- log(sapply(trees, function(x) length(x$tip.label)))
 library(ggplot2)
 ggplot(data = data.frame(div_rate, log_tips), aes(x= log_tips, y= div_rate))+
-  geom_point()
+  geom_point()+
   xlab("Log Number of Tips")+
   ylab("Net Diversification")+
   ggtitle("Net Diversification vs. Log Number pf Tips")
@@ -93,7 +53,7 @@ for( i in 1:100){
 }
 #Question8
 mean_traits <- sapply(traits, mean)
-cor(mean_trait, rates)
+cor(mean_traits, rates)
 # -0.08890789, negative  correlation present.
 plot(rates, mean_traits, xlab="Rates", ylab="Mean Trait")
 dev.off()
